c27b688bef9518424e40f25e55e476ea4c0f4e7a
Pedro Nunes Machado <100473453+PedroNnsM@users.noreply.github.com>
2022-08-18 14:29:59 -0300

    Dia 03/21

--- pages/dia3/index.js
+++ pages/dia3/index.js
@@ -1,6 +1,11 @@
 var $checkbox = document.querySelector('#checkbox');
 var $html = document.querySelector('html');
+var $claro = document.querySelector('#claro')
+var $escuro = document.querySelector('#escuro')
 
 $checkbox.addEventListener("click", function(event){
     $html.classList.toggle('dark-mode')
+    $claro.classList.toggle('invisivel')
+    $escuro.classList.toggle('invisivel')
+    
 } );
